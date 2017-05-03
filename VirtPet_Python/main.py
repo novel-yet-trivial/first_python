c71f6950120ffe5de4e73f27492be5fa3a27fd27
nyt <jonsspamaccout@gmail.com>
2017-05-03 15:48:39 -0700

    fixed animated gif

--- VirtPet_Python/main.py
+++ VirtPet_Python/main.py
@@ -1,39 +1,31 @@
+#!/usr/bin/env python3
+
 # Welcome to Creature Keeper
 # Python 3 code
 
-from tkinter import *
+import tkinter as tk
+from image_label import ImageLabel
 import tkinter.messagebox
 import time
 import os
 
 def mainScreen():
     # Window config options
-    ms = Tk()
+    ms = tk.Tk()
     ms.title("Creature Keeper")
     ms.config(bg="grey")
     ms.geometry("500x750")
     ms.resizable(width=False, height=False)
     ms.pack_propagate(0)
-    
-    # Placement of items in window
-    tLabel = Label(ms, text="Creature Keeper | Dev Edition", font=("Gill Sans", 28), bg="grey")
-    tLabel.place(x=250 , y=25, anchor="center")
-    
-    # Loop through the index of the animated gif
-    frame2 = [PhotoImage(file='images/ball-1.gif', format = 'gif -index %i' %i) for i in range(13)]
-    
-    def update(ind):
-        while ind <= 13:
-            frame = frame2[ind]
-            ind += 1
-            img.configure(image=frame)
-            ms.after(100, update, ind)
-            return ind
 
-    img = Label(ms)
+    # Placement of items in window
+    tLabel = tk.Label(ms, text="Creature Keeper | Dev Edition", font=("Gill Sans", 28), bg="grey")
+    tLabel.place(x=250 , y=25, anchor="center")
+
+    img = ImageLabel(ms)
+    img.load('images/ball-1.gif')
     img.place(x=250, y=250, anchor="center")
 
-    ms.after(0, update, 0)
     ms.mainloop()
 
 mainScreen()
